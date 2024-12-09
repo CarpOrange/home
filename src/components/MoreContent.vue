78e38cf0cd8bd36c53cbaf9eadfab8204346ceaa
LiYuchen <987424501@qq.com>
2024-12-09 21:02:49 +0800

    first commit

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">一棵勇敢的小草，自由地生长</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
